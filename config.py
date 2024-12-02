b59eec33c81d20caf841cd8bdaa46869f8561cae
Amarsingh008 <amarsinghrj92@gmail.com>
2024-12-02 03:14:36 -0800

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1114789110').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002168191467'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
